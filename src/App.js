3502602a4d962951807122a9ce3ab1b98ca841a3
RowAxl <rowaxl0@gmail.com>
2019-03-17 11:17:04 +0900

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 import UseLocation from './UseLocation'
